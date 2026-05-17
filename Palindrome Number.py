fca1b28c3d5e982a6e756de32263489dabe6b844
Ashu <ashuyadav210905@gmail.com>
2026-05-17 17:25:37 +0530

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -25,4 +25,18 @@ class Solution:
         # if s[::-1] != s:
         #     return False
         # return True
+
+Or 
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp = x
+        rev = 0
+
+        while temp > 0:
+            r = temp % 10
+            temp //= 10
+            rev = rev * 10 + r
+
+        return rev == x
 '''
