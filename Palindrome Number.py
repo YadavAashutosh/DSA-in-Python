1b92224db60a3d3c01003ac25f06e15c854fbe75
Ashu <ashuyadav210905@gmail.com>
2026-05-17 17:11:13 +0530

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def ispalindrome(self,x : int)->bool:
+        if x<0:
+            return False
+        y = list(map(int,str(x)))
+        m=1
+        for i in range(len(y)//2):
+            if y[i]!=y[-m]:
+                return False
+            m+=1
+        return True
+ans = Solution()
+print(ans.ispalindrome(121))
+print(ans.ispalindrome(-121))
+print(ans.ispalindrome(10))
+
+
+'''
+Best way 
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        return s == s[::-1]
+        # if s[::-1] != s:
+        #     return False
+        # return True
+'''
