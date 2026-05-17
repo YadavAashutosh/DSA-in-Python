470afd666e38d5bf6d58c7480af11fee56b32d52
Ashu <ashuyadav210905@gmail.com>
2026-05-17 21:05:17 +0530

    Create kidsWithmaxCandies.py

---
+++ kidsWithmaxCandies.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        ans = []
+        for i in candies:
+            ans.append( i + extraCandies >= max(candies))
+        return ans 
+a= Solution()
+print(a.kidsWithCandies([2,3,5,1,3],3))
+print(a.kidsWithCandies([4,2,1,1,2],1))
+print(a.kidsWithCandies([12,1,12],10))
+
