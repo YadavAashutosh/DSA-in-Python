cac3047b7d6b4782b7a9b8d3407c1f70b003270d
Ashu <ashuyadav210905@gmail.com>
2026-05-17 16:20:02 +0530

    Create Count the Digits That Divide a Number.py

---
+++ Count the Digits That Divide a Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countn(self ,num :int)->int:
+        count=0
+        n=num
+        while n>0:
+            r=n%10
+            if num%r==0:
+                count+=1
+            n//=10
+        return count
+answer = Solution()
+print(answer.countn(7))
+print(answer.countn(121))
+print(answer.countn(1248))
