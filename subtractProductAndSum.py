df0b50ed61264cd7ceb708638bb385b0c8f6f6d9
Ashu <ashuyadav210905@gmail.com>
2026-05-17 20:52:01 +0530

    Create subtractProductAndSum.py

---
+++ subtractProductAndSum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def subtractProductAndSum(self,n : int)->int:
+        a = list(map(int,str(n)))
+        b = 1
+        c = 0 
+
+        for i in a:
+            b *= i
+            c += i
+        
+        return b-c
+
+ans = Solution()
+print(ans.subtractProductAndSum(234))
+print(ans.subtractProductAndSum(4421))
+
+'''
+Another method :
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        temp = n
+        sum_ = 0
+        product = 1
+
+        while temp>0:
+            r = temp%10
+            temp//=10
+            sum_+=r
+            product*=r
+
+        return product-sum_
+
+'''
