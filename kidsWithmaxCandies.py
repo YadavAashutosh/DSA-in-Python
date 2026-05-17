1eb57d27f861da8a1a64dd76c70d0d8955a54640
Ashu <ashuyadav210905@gmail.com>
2026-05-17 21:13:53 +0530

    Update kidsWithmaxCandies.py

--- kidsWithmaxCandies.py
+++ kidsWithmaxCandies.py
@@ -9,3 +9,6 @@ print(a.kidsWithCandies([2,3,5,1,3],3))
 print(a.kidsWithCandies([4,2,1,1,2],1))
 print(a.kidsWithCandies([12,1,12],10))
 
+# [True, True, True, False, True]
+# [True, False, False, False, False]
+# [True, False, True]
