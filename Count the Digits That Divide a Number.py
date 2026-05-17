d438a7014177b0023e66e3d7b21b2acc032f22bc
Ashu <ashuyadav210905@gmail.com>
2026-05-17 16:21:03 +0530

    Update Count the Digits That Divide a Number.py

--- Count the Digits That Divide a Number.py
+++ Count the Digits That Divide a Number.py
@@ -12,3 +12,15 @@ answer = Solution()
 print(answer.countn(7))
 print(answer.countn(121))
 print(answer.countn(1248))
+
+# Another method
+# class Solution:
+#     def countDigits(self, num: int) -> int:
+#         count = 0
+#         n = list(map(int, str(num)))
+
+#         for i in n:
+#             if num%i==0:
+#                 count+=1
+#         return count
+    
